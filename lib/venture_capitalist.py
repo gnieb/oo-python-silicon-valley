ef3d40fab882d3a36dca74b428a394e688db59ef
Grace Nieboer <grace.nieboer@outlook.com>
2023-03-20 17:14:41 -0400

    VC model complete

--- lib/venture_capitalist.py
+++ lib/venture_capitalist.py
@@ -2,4 +2,14 @@ from lib.funding_round import *
 from lib.startup import *
 
 class VentureCapitalist:
-    pass
+    
+    all = []
+    
+    def __init__(self, name, number,):
+        self.name = name
+        self.total_worth = number
+        VentureCapitalist.all.append(self)
+   
+    @classmethod
+    def tres_commas_club(cls):
+        return [v for v in cls.all if v.total_worth > 1000000000 ]
