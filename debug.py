816700dc4e8b0f624eee021d1269ec7e3958f477
Grace Nieboer <grace.nieboer@outlook.com>
2023-03-20 17:21:15 -0400

    funding round model complete

--- debug.py
+++ debug.py
@@ -9,7 +9,7 @@ import ipdb;
 s1 = Startup( 'Pied Piper', 'Richard Hendricks', 'www.pp.com' )
 vc1 = VentureCapitalist( 'Peter Gregory', 100000000 )
 vc2 = VentureCapitalist('Erlich Baughman', 2000000000)
-#   fr1 = FundingRound( s1, vc1, 'Pre-Seed', 200000.99 )
+fr1 = FundingRound( s1, vc1, 'Pre-Seed', 200000.99 )
 
 
 
--- lib/funding_round.py
+++ lib/funding_round.py
@@ -1,2 +1,9 @@
 class FundingRound:
-    pass
+    all = []
+    def __init__(self, startup_object, venture_capitalist_object, type, investment ):
+        self._startup = startup_object
+        self._venture_capitalist = venture_capitalist_object
+        self.type = type
+        self.investment = investment
+        FundingRound.all.append(self)
+
