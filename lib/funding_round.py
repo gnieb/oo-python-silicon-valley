07db375ee6588787fb38393276356d21ebe045a9
Grace Nieboer <grace.nieboer@outlook.com>
2023-03-22 12:37:21 -0400

    basic fundinground setup

--- lib/funding_round.py
+++ lib/funding_round.py
@@ -3,8 +3,8 @@ class FundingRound:
     all = []
 
     def __init__(self, s_inst, vc, type, investment):
-        self.startup = s_inst
-        self.vc = vc
+        self._startup = s_inst
+        self._vc = vc
         self.type = type
-        self.invest = investment
+        self.invest = abs(investment)
         FundingRound.all.append(self)
